4b40d7e35eb36775526ae7008bf6c13805c3ea52
Christopher Quispe Calderon <forevertamer007@gmail.com>
2025-05-19 22:16:33 -0500

    Commit de prueba backend

---
+++ src/main/java/edu/pe/demale/demale_proyecto/DemaleProyectoApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.demale.demale_proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemaleProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemaleProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pe/demale/demale_proyecto/DemaleProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.demale.demale_proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemaleProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
